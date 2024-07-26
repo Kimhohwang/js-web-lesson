dfef274d3fb3b8cedde15f99bcdfbfef2f5978a1
Kimhohwang <fodric@ontactkorea.net>
2024-07-26 12:59:03 +0900

    test

--- lesson/src/lesson/04_함수/01_문법.jsx
+++ lesson/src/lesson/04_함수/01_문법.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Index0401() {
-  // 사용
+  // 사용 ddd
   function sayHello() {
     return "Hello";
   }
@@ -21,7 +21,8 @@ function Index0401() {
         <div>유사한 동작을 하는 코드가 여러 곳에서 필요한 경우 사용</div>
         <div>덕분에 중복 없이 유사한 동작을 하는 코드를 여러번 호출 가능</div>
         <div>
-          ex: 상품의 무게와 고객의 나라, postal code를 입력 받아서 배송 운임료를 계산하는 함수
+          ex: 상품의 무게와 고객의 나라, postal code를 입력 받아서 배송 운임료를
+          계산하는 함수
         </div>
         <a href="https://half-straw-a69.notion.site/05-Function-14aa963efacb4bf1a5365906a84f1300?pvs=4">
           이론 자료
@@ -29,7 +30,9 @@ function Index0401() {
 
         <div className="title">사용</div>
         <div>sayHello()의 실행 결과: {sayHello()}</div>
-        <div>sayHelloToFriend("Kim")의 실행 결과: {sayHelloToFriend("Kim")}</div>
+        <div>
+          sayHelloToFriend("Kim")의 실행 결과: {sayHelloToFriend("Kim")}
+        </div>
         <div>sum(1, 2)의 실행 결과: {sum(1, 2)}</div>
       </div>
     </div>
